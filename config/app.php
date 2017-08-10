8f4de0a69c8e6b6433c591d1bf0d9bc50f97a2e7
DESKTOP-U4VHMDR\Optimax Global <naykeng1kz1@gmail.com>
2017-08-10 17:51:41 +0700

    Section 34 Part1-Dwnloading packages and installing editor is done ===> continue to part 2 Referent: https://www.tinymce.com/ https://github.com/UniSharp/laravel-filemanager/blob/master/docs/installation.md http://image.intervention.io/getting_started/installation

--- config/app.php
+++ config/app.php
@@ -158,6 +158,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Unisharp\Laravelfilemanager\LaravelFilemanagerServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
     ],
 
     /*
@@ -205,6 +207,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
